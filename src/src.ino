54ff9a1fa40876efdf4a882c9bed4be5ca2551e4
jialong.kuang <jialong.kuang@ubtrobot.com>
2018-08-07 10:26:33 +0800

    优化灰度

--- src/src.ino
+++ src/src.ino
@@ -1,59 +1,30 @@
-#include "uKitExploreBlockly2.h"
+#include "uKitExplore2.h"
 
 void setup() {
     Initialization();
 }
 
 void loop() {
-    unsigned char Rvalue1=readColorRgb(1,'R');
-    unsigned char Gvalue1=readColorRgb(1,'G');
-    unsigned char Bvalue1=readColorRgb(1,'B');
-    Serial.println(Rvalue1);
-    Serial.println(Gvalue1);
-    Serial.println(Bvalue1);
-    if (readColor(1,"Red")) {
-        Serial.println("红色");
+    if (readGrayValue(1,0)) {
+        Serial.print("1");
 
     }
-    if (readColor(1,"Green")) {
-        Serial.println("绿色");
+    if (readGrayValue(2,0)) {
+        Serial.print("2");
 
     }
-    if (readColor(1,"Blue")) {
-        Serial.println("蓝色");
+    if (readGrayValue(3,0)) {
+        Serial.print("3");
 
     }
-    if (readColor(1,"Yellow")) {
-        Serial.println("黄色");
+    if (readGrayValue(4,0)) {
+        Serial.print("4");
 
     }
-    if (readColor(1,"Cyan")) {
-        Serial.println("青色");
+    if (readGrayValue(5,0)) {
+        Serial.print("5");
 
     }
-    if (readColor(1,"Purple")) {
-        Serial.println("紫色");
-
-    }
-    if (readColor(1,"Orange")) {
-        Serial.println("橙色");
-
-    }
-    if (readColor(1,"Black")) {
-        Serial.println("黑色");
-
-    }
-    if (readColor(1,"White")) {
-        Serial.println("白色");
-
-    }
-    if (readColor(1,"Gray")) {
-        Serial.println("灰色");
-
-    }
-    delay(1000);
+    delay(200);
 
 }
-
-
-
